b2b21b7e9325f1942014312c0b47b6fd3413c2dc
Paul <paulrousset@hotmail.fr>
2022-08-30 11:12:45 +0200

    refactor code and handling exceptions

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,7 @@
 import click.testing
 import pytest
 from hypermodern_python_example import console
+import requests
 
 
 @pytest.fixture
@@ -21,3 +22,37 @@ def mock_requests_get(mocker):
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+# testing that the title returned by the API is printed to the console
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+
+# testing that requests.get is invoked
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
+
+# testing the URL passed to requests.get
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+
+# testing that the program exits with a status code of 1 on request errors
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
+
+
+# testing the message error
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
+
