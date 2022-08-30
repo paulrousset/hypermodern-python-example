ea8a662215a7b03f757c88ce04050264b10b4ec0
Paul <paulrousset@hotmail.fr>
2022-08-30 11:23:30 +0200

    create conftest.py file to store test fixture

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,6 +1,7 @@
 from hypermodern_python import wikipedia
 
 
+# testing that the language option is working
 def test_random_page_uses_given_language(mock_requests_get):
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
