9d22f630ace410fe247c6253ea3a5b0665c13d1e
Paul <paulrousset@hotmail.fr>
2022-08-31 15:21:29 +0200

    add docstrings

--- src/hypermodern_python_example/wikipedia.py
+++ src/hypermodern_python_example/wikipedia.py
@@ -1,10 +1,33 @@
+"""Client for the Wikipedia REST API, version 1."""
+
 import click
 import requests
+
+@dataclass
+class Page:
+    """Page resource.
+    Attributes:
+        title: The title of the Wikipedia page.
+        extract: A plain text summary.
+    """
+    title: str
+    extract: str
 
 API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page(language="en"):
+def random_page(language: str = "en") -> Page:
+    """Return a random page.
+    Performs a GET request to the /page/random/summary endpoint.
+    Args:
+        language: The Wikipedia language edition. By default, the
+            English Wikipedia is used ("en").
+    Returns:
+        A page resource.
+    Raises:
+        ClickException: The HTTP request failed or the HTTP response
+            contained an invalid body.
+"""
     url = API_URL.format(language=language)
     try:
         with requests.get(url) as response:
