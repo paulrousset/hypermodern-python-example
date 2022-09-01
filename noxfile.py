7a24eaf5ca9c7a289b68aad5b035f2b1d72cd074
Paul <paulrousset@hotmail.fr>
2022-09-01 11:56:21 +0200

    clean noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,4 @@
 """ Nox sessions"""
-import tempfile
-from typing import Any
 
 import nox
 from nox.sessions import Session
@@ -13,110 +11,63 @@ nox.options.sessions = "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py", "docs/conf.py"
 
 
-# @nox.session(python=["3.10", "3.9"])
 @session(python=["3.10", "3.9"])
 def tests(session: Session) -> None:
     """Run the test suite"""
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
-    # install_with_constraints(session,
-    #                          "coverage[toml]",
-    #                          "pytest",
-    #                          "pytest-cov",
-    #                          "pytest-mock")
-    session.install("pytest", ".")
+    session.install("coverage[toml]",
+                    "pytest",
+                    "pytest-cov",
+                    "pytest-mock",
+                    ".")
     session.run("pytest", *args)
 
 
-# @nox.session(python=["3.10", "3.9"])
 @session(python=["3.10", "3.9"])
 def lint(session: Session) -> None:
     """Lint using flake8"""
     args = session.posargs or locations
     session.install(
         "flake8",
+        "flake8-annotations",
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
+        "flake8-docstrings",
         "flake8-import-order",
+        "darglint",
     )
-    # install_with_constraints(
-    #     session,
-    #     "flake8",
-    #     "flake8-annotations",
-    #     "flake8-bandit",
-    #     "flake8-black",
-    #     "flake8-bugbear",
-    #     "flake8-docstrings",
-    #     "flake8-import-order",
-    #     "darglint",
-    # )
     session.run("flake8", *args)
 
 
-# @nox.session(python="3.10")
 @session(python=["3.10"])
 def black(session: Session) -> None:
     """Run Black code formatter"""
     args = session.posargs or locations
     session.install("black")
-    # install_with_constraints(session, "black")
     session.run("black", *args)
 
 
-# @nox.session(python="3.10")
 @session(python=["3.10"])
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages"""
     session.install("safety")
-    # install_with_constraints(session, "safety")
     session.run("safety", "check", "--full-report")
 
 
-# def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
-#     """Install packages constrained by Poetry's lockfile"""
-#     with tempfile.NamedTemporaryFile() as requirements:
-#         session.run(
-#             "poetry",
-#             "export",
-#             "--dev",
-#             "--format=requirements.txt",
-#             f"--output={requirements.name}",
-#             external=True,
-#         )
-#         session.install(f"--constraint={requirements.name}", *args, **kwargs)
-
-# import os
-
-# def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
-#     """Install packages constrained by Poetry's lockfile"""
-#     requirements = os.path.join("C:", "test")
-#     session.run(
-#             "poetry",
-#             "export",
-#             "--dev",
-#             "--format=requirements.txt",
-#             f"--output={requirements}",
-#             external=True,
-#     )
-#     session.install(f"--constraint={requirements}", *args, **kwargs)
-
-# @nox.session(python=["3.10", "3.9"])
 @session(python=["3.10", "3.9"])
 def xdoctest(session: Session) -> None:
     """Run examples with xdoctest."""
     args = session.posargs or ["all"]
     session.run("poetry", "install", "--no-dev", external=True)
-    # install_with_constraints(session, "xdoctest")
     session.install("xdoctest")
     session.run("python", "-m", "xdoctest", package, *args)
 
 
-# @nox.session(python="3.10")
 @session(python=["3.10"])
 def docs(session: Session) -> None:
     """Build the documentation."""
     session.run("poetry", "install", "--no-dev", external=True)
-    # install_with_constraints(session, "sphinx", "sphinx-autodoc-typehints")
     session.install("sphinx", "sphinx-autodoc-typehints")
     session.run("sphinx-build", "docs", "docs/_build")
